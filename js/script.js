b93ffe201acf01aed3b9ff91f1b0e9726e3a8ecf
Gimedeiros-77 <giovanimedeiros2@gmail.com>
2025-02-15 09:05:41 -0300

    Projeto finalizado

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("insta").addEventListener('click', function(){
+    window.open("https://www.instagram.com/felipe_lacroes?igsh=MWdyamNneHMxeXNwZw==")
+});
+
+document.getElementById("wpp").addEventListener('click', function(){
+    window.open("https://wa.me/5517992612344?text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento!")
+});
+
+
